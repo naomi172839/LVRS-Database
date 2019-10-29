226349ca94d5904e1067c7a483c77ed5cb61fc20
Naomi <naomi172839@gmail.com>
2019-10-29 16:55:41 -0400

    Added Initial Submit Logic for some items

--- LVRS Database/AddMemberDetails.xaml.cs
+++ LVRS Database/AddMemberDetails.xaml.cs
@@ -49,5 +49,30 @@ namespace LVRS_Database
         {
             clearAll();
         }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            LVRSMEMBERSEntities context = new LVRSMEMBERSEntities();
+            int memID = Int32.Parse(memberID.Text);
+            if (birthDate.Text != "")
+            {
+                birthday tempBirthday = new birthday();
+                tempBirthday.memberID = memID;
+                tempBirthday.birthDate = DateTime.Parse(birthDate.Text);
+                context.birthdays.Add(tempBirthday);
+            }
+            address tempAddress = new address();
+            tempAddress.memberID = memID;
+            tempAddress.addressStreet = addressStreet.Text;
+            tempAddress.addressCity = addressCity.Text;
+            tempAddress.addressState = addressState.Text;
+            tempAddress.addressZip = Int32.Parse(addressZip.Text);
+
+            context.addresses.Add(tempAddress);
+            
+            context.SaveChanges();
+
+
+        }
     }
 }
